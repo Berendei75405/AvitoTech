6597919106b223a9190adb0d1bb66b837012a857
Novgorodtcev <73novgor@mail.ru>
2024-11-19 16:10:47 +0700

    Добавления отображения ошибки

--- AvitoTech/MainModule/View/MainViewController.swift
+++ AvitoTech/MainModule/View/MainViewController.swift
@@ -12,6 +12,8 @@ final class MainViewController: UIViewController {
     var viewModel: MainViewModel?
     private var cancellabele = Set<AnyCancellable>()
     
+    private var centerYConstraint: NSLayoutConstraint!
+    
     //MARK: - deinit
     deinit {
         print("MainViewController")
@@ -39,14 +41,69 @@ final class MainViewController: UIViewController {
             case .initial:
                 print("Таблица инициализированна")
             case .success:
+                UIView.animate(withDuration: 0.8,
+                               delay: 0,
+                               options: .curveEaseInOut,
+                               animations: {
+                    self.centerYConstraint.constant = 600
+                    self.view.layoutIfNeeded()
+                })
                 tableView.reloadData()
                 tableView.delegate = self
                 tableView.dataSource = self
+                
             case .failure(let error):
-                print("Error: \(error)")
+                UIView.animate(withDuration: 0.8,
+                               delay: 0,
+                               options: .curveEaseInOut,
+                               animations: {
+                    self.centerYConstraint.constant = 0
+                    self.view.layoutIfNeeded()
+                })
+                switch error {
+                    case .errorWithDescription(let stringError):
+                        errorLabel.text = stringError
+                    case .error(let error):
+                        errorLabel.text = error.localizedDescription
+                }
             }
         }
     }
+    
+    //MARK: - errorView
+    private let errorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        view.layer.cornerRadius = 15
+        view.alpha = 1
+        
+        return view
+    }()
+    
+    //MARK: - errorLabel
+    private let errorLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = .boldSystemFont(ofSize: 20)
+        label.lineBreakMode = .byWordWrapping
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    //MARK: - closeButton
+    private let closeButton: UIButton = {
+        var but = UIButton()
+        but.translatesAutoresizingMaskIntoConstraints = false
+        but.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        but.setImage(UIImage(systemName: "xmark"), for: .normal)
+        but.tintColor = .black
+        but.layer.cornerRadius = 5
+        
+        return but
+    }()
 
     //MARK: - viewDidLoad
     override func viewDidLoad() {
@@ -78,6 +135,67 @@ final class MainViewController: UIViewController {
             tableView.bottomAnchor
                 .constraint(equalTo: view.bottomAnchor)
         ])
+        
+        //errorView
+        view.addSubview(errorView)
+        centerYConstraint = errorView.centerYAnchor.constraint(
+            equalTo: view.centerYAnchor, constant: 0)
+        NSLayoutConstraint.activate([
+            errorView.centerXAnchor.constraint(
+                equalTo: view.centerXAnchor),
+            centerYConstraint,
+            errorView.widthAnchor.constraint(
+                equalToConstant: view.frame.width - 64),
+            errorView.heightAnchor.constraint(
+                equalToConstant: view.frame.height/2)
+        ])
+        
+        //errorLabel
+        errorView.addSubview(errorLabel)
+        NSLayoutConstraint.activate([
+            errorLabel.topAnchor.constraint(
+                equalTo: errorView.topAnchor,
+                constant: 32),
+            errorLabel.leadingAnchor.constraint(
+                equalTo: errorView.leadingAnchor,
+                constant: 8),
+            errorLabel.trailingAnchor.constraint(
+                equalTo: errorView.trailingAnchor,
+                constant: -8),
+            errorLabel.bottomAnchor.constraint(
+                equalTo: errorView.bottomAnchor,
+                constant: -32)
+        ])
+        
+        //closeButton
+        errorView.addSubview(closeButton)
+        closeButton.addTarget(self, action: #selector(closeButtonAction), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(
+                equalTo: errorView.topAnchor,
+                constant: 8),
+            closeButton.trailingAnchor.constraint(
+                equalTo: errorView.trailingAnchor,
+                constant: -8),
+            closeButton.heightAnchor.constraint(
+                equalToConstant: 35),
+            closeButton.widthAnchor.constraint(
+                equalToConstant: 35)
+        ])
+        
+    }
+    
+    //MARK: - closeButtonAction
+    @objc private func closeButtonAction() {
+        UIView.animate(withDuration: 0.8,
+                       delay: 0,
+                       options: .curveEaseInOut,
+                       animations: {
+            self.centerYConstraint.constant = 600
+            self.view.layoutIfNeeded()
+        })
+        viewModel?.fetchEmployees()
     }
     
 }
